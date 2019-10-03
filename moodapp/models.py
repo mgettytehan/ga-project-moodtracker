2830d92287ba908e7321f9a2d10c8a68dfad99d7
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-10-03 15:12:10 -0400

    Began to add core fields for DB models. User and its foreign keys in other entities not yet set up

---
+++ moodapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class MoodScale(models.Model):
+    scaleName = models.CharField(max_length=30)
+    scaleType = models.CharField(max_length=15)
+
+class ScaleItem(models.Model):
+    class Meta:
+        unique_together = ['moodScale', 'index']
+
+    index = models.PositiveSmallIntegerField()
+    alias = models.CharField(max_length=25)
+    moodScale = models.ForeignKey('MoodScale', on_delete=models.CASCADE, related_name='scaleItems')
+
+class MoodLog(models.Model):
+    madeOn = models.DateTimeField(auto_now_add=True)
+    notes = models.CharField(max_length=400)
+
+class MoodInMoodLog(models.Model):
+    class Meta:
+        unique_together = ['moodLog', 'scaleItem']
+    
+    moodLog = models.ForeignKey('MoodLog', on_delete=models.CASCADE, related_name='moodInMoodLogs')
+    scaleItem = models.ForeignKey('ScaleItem', on_delete=models.CASCADE, related_name='moodInMoodLogs')

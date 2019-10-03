3b91b1d6fbe7069485c7e10510184dab2fa5a043
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-10-03 10:54:23 -0400

    Fixed settings for heroku

--- project4/settings.py
+++ project4/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +127,5 @@ REACT_APP_DIR = "client"
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static')
 ]
+
+django_heroku.settings(locals())

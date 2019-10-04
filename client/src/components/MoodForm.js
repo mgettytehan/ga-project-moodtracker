9137c4cbe439481171ce124650348a0cc18c05a4
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-10-03 22:18:10 -0400

    Added basic components for mood logging form

---
+++ client/src/components/MoodForm.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+// data for testing
+const testMoodScales = [
+    {
+        id: 1,
+        scaleName: "Happiness",
+        scaleType: "number",
+        scaleItems: [
+            {
+                index: 0,
+                alias: 1,
+                moodScale: 1
+            },
+            {
+                index: 1,
+                alias: 2,
+                moodScale: 1
+            }
+        ]
+    },
+    {
+        id: 2,
+        scaleName: "Gumption",
+        scaleType: "number",
+        scaleItems: [
+            {
+                index: 0,
+                alias: 1,
+                moodScale: 1
+            },
+            {
+                index: 1,
+                alias: 2,
+                moodScale: 1
+            },
+            {
+                index: 2,
+                alias: 3,
+                moodScale: 1
+            }
+        ]
+    },
+]
+
+const moodScaleDisplay = moodScale => {
+    return (
+        <div>
+            <span>{moodScale.scaleName}</span>
+            {moodScale.scaleItems.map(scaleItem =>
+                (<label>
+                <input type="radio" name={moodScale.id} value={scaleItem.index} />
+                    {scaleItem.alias}
+                </label>))
+            }
+        </div>);
+}
+
+const moodForm = () => {
+    return (
+        <div>
+            <form>
+                {testMoodScales.map(moodScale => moodScaleDisplay(moodScale))}
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
+}
+
+export { moodForm };

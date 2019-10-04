a03ee00577e2a7c511dc15b1e04d383b69ed5868
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-10-04 13:55:22 -0400

    Began to add JSON web tokens for authentication/authorization

--- project4/settings.py
+++ project4/settings.py
@@ -128,4 +128,15 @@ STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static')
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
 django_heroku.settings(locals())
--- project4/urls.py
+++ project4/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from . import views
+from rest_framework_jwt.views import obtain_jwt_token
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token-auth/', obtain_jwt_token),
     re_path(r'^.*$', views.FrontendAppView.as_view()),
 ]
